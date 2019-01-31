9d769b32f863b63f3a6054273b2ef87dc52d0f0c
Elliot Chen <lltchen1@gmail.com>
2019-01-31 19:37:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
 
     def add_item(title,price,opi = nil)
       self.total += price
-        return total = (price * opi)
+        return seld.total = (price * opi)
          binding.pry
       end
     end
