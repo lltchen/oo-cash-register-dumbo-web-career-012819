210c456ffdefa2a4796ccc9e64346092f127cc2c
Elliot Chen <lltchen1@gmail.com>
2019-01-31 19:18:44 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+require "pry"
 
+
+class CashRegister
+
+    attr_accessor  :discount, :total
+
+    def initialize(discount = 0)
+      @total = 0
+      @discount = discount
+    end
+    def add_item(title,price,opi=nil)
+      self.total += price
+      if opi != nil
+        exit!
+        total += (price * opi)
+      end
+    end
+
+end
